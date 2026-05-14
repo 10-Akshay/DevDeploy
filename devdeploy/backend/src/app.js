b2be2b079a59e57dc31360ceddb27cf3eb5f3c6e
Akshay Sutar <akshayrsutar358@gmail.com>
2026-05-14 11:37:53 +0530

    Added MongoDB and register authentication API

--- devdeploy/backend/src/app.js
+++ devdeploy/backend/src/app.js
@@ -4,9 +4,15 @@ const express = require("express");
 
 const connectDB = require("../config/db");
 
+const authRoutes = require("./routes/auth");
+
 const app = express();
 
 connectDB();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(4000, () => {
   console.log("DevDeploy backend running on http://localhost:4000");
