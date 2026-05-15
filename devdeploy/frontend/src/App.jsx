7df8eefd7ba68fd2baf9d685e51cfc93092e6951
Akshay Sutar <akshayrsutar358@gmail.com>
2026-05-15 17:59:08 +0530

    update frontend with good UI

--- devdeploy/frontend/src/App.jsx
+++ devdeploy/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -21,7 +22,14 @@ function App() {
 
         <Route path="/signup" element={<Signup />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
 
       </Routes>
 
--- devdeploy/frontend/src/pages/login.jsx
+++ devdeploy/frontend/src/pages/login.jsx
@@ -1,4 +1,52 @@
+import { useState } from "react";
+
+import axios from "axios";
+
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      const response = await axios.post(
+        "http://localhost:4000/api/auth/login",
+        formData
+      );
+
+      localStorage.setItem(
+        "token",
+        response.data.token
+      );
+
+      alert("Login Successful 🚀");
+
+      navigate("/dashboard");
+
+    } catch (error) {
+
+      alert("Login Failed");
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-950 flex items-center justify-center px-4">
 
@@ -8,21 +56,31 @@ function Login() {
           Login
         </h1>
 
-        <form className="flex flex-col gap-5">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5"
+        >
 
           <input
             type="email"
+            name="email"
             placeholder="Enter Email"
+            value={formData.email}
+            onChange={handleChange}
             className="p-4 rounded-xl bg-gray-800 text-white outline-none border border-gray-700 focus:border-blue-500"
           />
 
           <input
             type="password"
+            name="password"
             placeholder="Enter Password"
+            value={formData.password}
+            onChange={handleChange}
             className="p-4 rounded-xl bg-gray-800 text-white outline-none border border-gray-700 focus:border-blue-500"
           />
 
           <button
+            type="submit"
             className="bg-blue-600 hover:bg-blue-700 transition text-white p-4 rounded-xl font-semibold text-lg"
           >
             Login
